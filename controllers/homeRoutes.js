84260c5c45e96fa1929277165918f04e4286fe09
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-08 18:29:49 -0500

    added GET for homeRoute to display existing posts if logged in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,2 +1,24 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models')
+
+
 // displays existing blog posts
+// GET all posts for homepage - if logged in
+router.get('/', async (req, res) => {
+    try {
+        const dbPostData = await Post.findAll();
+
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+
 // nav links for homepage, dashboard, log in
