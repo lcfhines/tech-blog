8213fde3b82696273e56a529401ba3beab5af390
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-07 19:50:25 -0500

    added hooks to hash password before create and before update

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 class User extends Model {}
 
@@ -26,8 +27,20 @@ User.init(
             len: [8,30],
         }
     }
+}, 
+{ 
+    // add hooks to hash password before creating and before updtaing
+    hooks: {
+        beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        beforeUpdate: async (updatedUserData) => {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
 
-}, {
+},
     sequelize,
     timestamps: false,
     freezeTableName: true,
