c1bd3dc76c9b7671d2b56c371d68675b33cf91d2
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-07 20:06:59 -0500

    added post model

---
+++ models/Post.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+ {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [2,200]
+        }
+    },
+    contents: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [2,5000]
+        }
+    },
+    // reference username of the creator of the post
+    creator_username: {
+        type: DataTypes.STRING,
+        references: {
+            model: 'user',
+            key: 'username',
+        },
+
+    },
+
+}, 
+{
+    sequelize,
+    // change timestamps to true to include createdAt and updatedAt columns
+    timestamps: true,
+    freezeTableName: true,
+    modelName: 'post'
+}
+);
+
+module.exports = Post;
