9ef3fb81d7c32acd73ba08a00ad63714030ce78f
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-08 18:21:19 -0500

    added dependencies and set up to server.js for express, helpers, handlebars, and session

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
 // dependencies
+const path = require('path');
 const express = require('express');
 const session = require ('express-session');
-const routes = require('./controllers');
 const exphbs = require('express-handlebars');
-const path = require('path');
-const hbs = exphbs.create({});
 
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('.utils/helpers');
 
 // set up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// assign hbs.engine to label 'handlebars' 
+// set up sessions
+const sess = {
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(require(routes));
 
-// Starts the server to begin listening
+// start the server
 app.listen(PORT, () => {
   console.log('Server listening on: http://localhost:' + PORT);
 });
