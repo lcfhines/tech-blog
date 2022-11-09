8d40bead4ed3c0e4a17019b79695ecedb4c96597
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-08 21:03:25 -0500

    added dashboard route to get all posts created by logged in user

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,8 +1,31 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models')
 
-// displays the posts i have created
-// displays option to create new post
+// GET all posts logged in user has created
+router.get('/dashboard', async (req, res) => {
+    try {
+        const dbPostData = await Post.findAll({
+            where: {
+                username: req.session.username
+            }
+        })
+        const post = dbPostData.get({ plain: true });
+        res.render('post', {post, loggedIn: req.session.loggedIn});
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+    });
+
+
+// CREATE post
+
+
+// UPDATE post
+
+
+// DELETE post
+
 
 
 module.exports = router;
