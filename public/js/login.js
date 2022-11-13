e485a9ea6cf2dd961ea80e0953a9c471ccffec38
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-12 21:11:01 -0500

    updated fetch in login.js

--- public/js/login.js
+++ public/js/login.js
@@ -14,6 +14,7 @@ const loginFormHandler = async (event) => {
       if (response.ok) {
         document.location.replace('/');
       } else {
+        console.log(err);
         alert('Failed to log in.');
       }
     }
