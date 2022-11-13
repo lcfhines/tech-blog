44802e32d232a61f0805da29c5de0d6d0eb2cac3
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-12 20:59:12 -0500

    added route to get one post by id

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,4 +28,27 @@ router.get('/login', (req, res) => {
     } res.render('login');
 });
 
+
+// get one post
+router.get('/post/:id', async (req, res) => {
+  try {
+      const dbPostData = await Post.findByPk({
+        where: {
+          id: req.params.id
+        },
+      });
+
+      const post = dbPostData.map((post) => post.get({ plain: true }));
+
+      res.render('homepage', {
+          post,
+          loggedIn: req.session.loggedIn,
+      });
+  } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+  }
+});
+
+
 module.exports = router;
