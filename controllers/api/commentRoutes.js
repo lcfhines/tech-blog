dd8a1c7f77afc370b143082ac69c99cd997d5ea1
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-21 18:04:04 -0500

    added route for creating a comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,9 +2,26 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../../models')
 
 
-// get all comments for a post
+// get all comments
+router.get('/', (req, res) => {
+    Comment.findAll({})
+    .then(commentData => res.json(commentData))
+    .catch (err => {
+        res.status(500).json(err)
+    });
+});
 
 // create comment
-
+router.post('/', async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            userId: req.session.userId,
+        });
+        res.json(newComment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
